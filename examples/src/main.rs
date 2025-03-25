d8c565d8fd7b3df1b32f4d3f70bfa9f17b1221e3
sam <sam@pop-os.localdomain>
2025-03-25 14:10:39 -0500

    wip ui example

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,27 +1,39 @@
 use std::{env, net, thread, io};
 use std::ops::ControlFlow;
-use std::io::Read;
-use std::io::BufRead;
+// use std::io::Read;
+// use std::io::BufRead;
 mod error;
-use std::str::Lines;
+// use std::str::Lines;
+use slint::{Model, ModelRc, SharedString};
+use std::{rc::Rc, str::FromStr};
+
 use slint::PlatformError;
 
 slint::include_modules!();
 
 use argh::FromArgs;
-use client::{Loading, Client, Config};
+use client::{Loading, Client, Config,Event,Peer};
 mod logger;
 use nakamoto_cash::client::traits::Handle;
 use nakamoto_cash::client::{self, Network};
 // use nakamoto_cash::common::bitcoin::util::bloom::BloomFilter;
 type Reactor = nakamoto_cash::net::poll::Reactor<net::TcpStream>;
-use crossbeam_channel::{self as chan};
+use crossbeam_channel::{self as chan, Receiver, Sender};
 mod bloom;
 use bloom::{Bloom,BloomFilter};
 use std::io::Write;
+
+#[derive(Clone,Debug)]
+pub enum UIMessage {
+    HeaderLoaded(u64),
+    PeerHeightUpdate(u64),
+    AddBlooomItem(String)
+}
+
 pub struct Watcher<H> {
     client: H,
     network: Network,
+    peers: Vec<Peer>,
     bloom: BloomFilter,
 }
 
@@ -29,25 +41,29 @@ impl<H: Handle> Watcher<H> {
     pub fn new(client: H, network: client::Network) -> Self {
         let filter = Bloom::<u8>::new_for_fp_rate(10_000, 0.01);
         let bloom = BloomFilter::from(filter);
+        let peers: Vec<Peer> = Vec::with_capacity(32);
         Self {
             client,
             network,
+            peers,
             bloom,
         }
     }
 
-    /// Run the wallet loop until it exits.
+    /// Run the watch loop until it exits.
     pub fn run(
         &mut self,
-        loading: &chan::Receiver<client::Loading>,
-        events: &chan::Receiver<client::Event>,
-        ui_rx: &chan::Receiver<Vec<String>>,
+        loading: &Receiver<client::Loading>,
+        events: &Receiver<client::Event>,
+        ui_input_rx: &Receiver<UIMessage>,
+        ui_show_tx: &Sender<UIMessage>,
+
     ) -> Result<(), error::Error> {
         loop {
             chan::select! {
                 recv(loading) -> event => {
                     if let Ok(event) = event {
-                        if let ControlFlow::Break(()) = self.handle_loading_event(event)? {
+                        if let ControlFlow::Break(()) = self.handle_loading_event(event,ui_show_tx)? {
                             return Ok(());
                         }
                     } else {
@@ -57,17 +73,17 @@ impl<H: Handle> Watcher<H> {
                 recv(events) -> event => {
                     let event = event?;
                     if let ControlFlow::Break(()) = 
-                        self.handle_client_event(event)? { 
+                        self.handle_client_event(event,ui_show_tx)? { 
                         break;
                     }
                 }
-                // recv(ui_rx) -> event => {
-                //     let event = event?;
-                //     if let ControlFlow::Break(()) = 
-                //         self.handle_user_input(event)? { 
-                //         break;
-                //     }
-                // }
+                recv(ui_input_rx) -> event => {
+                    let event = event?;
+                    if let ControlFlow::Break(()) = 
+                        self.update_user_input(event)? { 
+                        break;
+                    }
+                }
             }
         }
         Ok(())
@@ -76,10 +92,15 @@ impl<H: Handle> Watcher<H> {
     fn handle_client_event(
         &mut self,
         event: client::Event,
+        ui_show_tx: &chan::Sender<UIMessage>,
     ) -> Result<ControlFlow<()>, error::Error> {
         match event {
-            client::Event::Ready { tip, .. } => {
-                log::info!(target: "watcher ready at tip", "{}", tip);
+            Event::Ready { tip, .. } => {
+                // log::info!(target: "watcher ready at tip", "{}", tip);
+            }
+            Event::PeerNegotiated {height, .. } => {
+                _ = ui_show_tx.send(UIMessage::PeerHeightUpdate(height))
+                
             }
             _ => {}  // Added catch-all for unhandled events
         }
@@ -89,25 +110,34 @@ impl<H: Handle> Watcher<H> {
     fn handle_loading_event(
         &mut self,
         event: client::Loading,
+        ui_show_tx: &chan::Sender<UIMessage>,
     ) -> Result<ControlFlow<()>, error::Error> {
         match event {
             Loading::BlockHeaderLoaded { height } => {
-                print!("\r HEADERS LOADING {}\r",height);
+                _ = ui_show_tx.send(UIMessage::HeaderLoaded(height))
+            }
+            Loading::BlockHeaderLoadComplete => {
+                //TODO inform UI?
             }
             _ => {}
         }
         Ok(ControlFlow::Continue(()))
     }
-    
-    fn handle_user_input(
+    fn update_user_input(
         &mut self,
-        event:Vec<String> ,
+        ui_input: UIMessage,
     ) -> Result<ControlFlow<()>, error::Error> {
-        for argument in env::args() {
-            println!("{argument}");
-}
+              println!("BLOOM ITEM RECV {:?}",ui_input) ;            
+            match ui_input {
+              UIMessage::AddBlooomItem(data) => {
+              println!("BLOOM ITEM RECV {:?}",data) ;            
+                }
+            UIMessage::HeaderLoaded(msg) => {}
+                _ => {},
+            }
         Ok(ControlFlow::Continue(()))
     }
+    
 }
 
 // Rest of your code remains the same...
@@ -140,6 +170,7 @@ fn main() {
     };
 
     let (loading_tx, loading_rx) = chan::unbounded();
+    let (ui_show_tx, ui_show_rx) = chan::unbounded();
     let (ui_input_tx, ui_input_rx) = chan::unbounded();
 
     let client_recv = handle.events();
@@ -159,6 +190,7 @@ fn main() {
             &loading_rx,
             &client_recv,
             &ui_input_rx,
+            &ui_show_tx,
         ) {
             println!("FATAL ERR {:?}", err);
             std::process::exit(1);
@@ -166,18 +198,89 @@ fn main() {
     });
 
     // New thread for handling user input from terminal
-    run_ui_main();
+    _ = run_ui_main(&ui_input_tx ,&ui_show_rx);
     t1.join().unwrap().unwrap();
     t2.join().unwrap();
     // t3.join().unwrap();
+}
+
+pub fn run_ui_main(
+        ui_input_tx: &Sender<UIMessage>,
+        ui_show_rx: &Receiver<UIMessage>,
+) -> Result<(), PlatformError> {
+        let filter_items = Rc::new(slint::VecModel::<SharedString>::from(vec![]));
+
+        let main_window = MainWindow::new()?;
+        let ui_show_rx = ui_show_rx.clone();
+
+        let app = main_window.as_weak().clone();
+        let tx_handle = ui_input_tx.clone();
+        
+        app.unwrap().on_load_bloom_item(move || {
+        let items = app.unwrap().get_filter_items();
+        let item = app.unwrap().get_bloom_item();
+        
+        _ = tx_handle.send(UIMessage::AddBlooomItem(item.clone() .into()));
+        println!("FILTER ITEM {:?}", item);
+        filter_items.push(item);
+        items.iter().for_each(|f| {
+            filter_items.push(f.clone());
+        });
+
+        println!("FILTER ITEMs {:?}", filter_items.row_count());
+        // let items = filter_items
+        //     .iter()
+        //     .map(|i| i.to_string())
+        //     .collect::<Vec<_>>();
+        // items.iter().for_each(|f| {
+        // });
+        // let payload = json::to_string(&items);
+
+        // let msg = Payload {
+        //     message: "load-item".to_string(),
+        //     payload,
+        // };
+        // _ = tx_handle.send(msg);
+    });
+
+
+
+        
+        let app = main_window.as_weak().clone();
+            std::thread::spawn(move || {
+            loop {
+                chan::select! {
+                    recv(ui_show_rx) -> event => {
+                        if let Ok(event) = event {
+                            _ = app .upgrade_in_event_loop(move |app| {
+                             match event {
+                                UIMessage::HeaderLoaded(height) => {
+                                    app.as_weak().unwrap().set_loaded_header(height.to_string().into());
+                                }
+                                UIMessage::PeerHeightUpdate(height) => {
+                                    app.as_weak().unwrap().set_loaded_header(height.to_string().into());
+                                }
+                                UIMessage::AddBlooomItem(item) => {
+                                    app.as_weak().unwrap().set_bloom_item(item.into()) 
+                                }
+                            }
+                                
+                            });
+
+                  
+                      }
+                   }
+                }
+
+            }                
+         });
+
+
+        main_window.run()
 }
 
 impl Options {
     pub fn from_env() -> Self {
         argh::from_env()
     }
-}
-pub fn run_ui_main() -> Result<(), PlatformError> {
-        let main_window = MainWindow::new()?;
-        main_window.run()
 }
